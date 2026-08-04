4db145dc5b0a7fa4a77d8de9af809657dee652f1
Anastasiia Parfeniuk <anastasiia.parfeniuk@hm.edu>
2026-08-04 19:01:42 +0200

    Rewrite the HandTracker to make it reusable for later projects

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -4,56 +4,115 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import time
 
-cap = cv2.VideoCapture(0)
+class HandTracker:
+    def __init__(self, model_path='hand_landmarker.task', _num_hands=2):
+        self.base_options = python.BaseOptions(model_asset_path=model_path)
+        self.options = vision.HandLandmarkerOptions(base_options=self.base_options, running_mode=vision.RunningMode.VIDEO, num_hands=_num_hands)
+        self.detector = vision.HandLandmarker.create_from_options(self.options)
+        self.HAND_CONNECTIONS = [
+            (0,1),(1,2),(2,3),(3,4),
+            (0,5),(5,6),(6,7),(7,8),
+            (5,9),(9,10),(10,11),(11,12),
+            (9,13),(13,14),(14,15),(15,16),
+            (13,17),(17,18),(18,19),(19,20),
+            (0,17)
+            ]
+        self.timestamp_ms = 0
+        self.result = None
+        self.landmarks = []
 
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options, running_mode=vision.RunningMode.VIDEO, num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
+    def detect_frame(self, frame):
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
+        self.timestamp_ms = int(time.time() * 1000)
+        self.result = self.detector.detect_for_video(mp_image, self.timestamp_ms)
+        self.landmarks = self._extract_landmarks()
 
-timestamp_ms = 0
+    # result = detector.detect_for_video(mp_image, timestamp_ms)
+
+    def _extract_landmarks(self):
+
+        landmarks = []
+        if self.result is None:
+            return landmarks
+
+        for hand in self.result.hand_landmarks:
+            hand_landmarks = []
+            for id, lm in enumerate(hand):
+                hand_landmarks.append({
+                    "id": id,
+                    "x": lm.x,
+                    "y": lm.y,
+                    "z": lm.z
+                })
+            landmarks.append(hand_landmarks)
+        return landmarks
+
+    def draw_landmarks(self, frame):
+        if self.result is None:
+            return frame
+        height, width, _ = frame.shape
+
+        for hand in self.result.hand_landmarks:
+                    for landmark in hand:
+                        x = int(landmark.x * width)
+                        y = int(landmark.y * height)
+        
+                        cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+                        cv2.putText(frame, str(id), (x+10, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
 
-HAND_CONNECTIONS = [
-    (0,1),(1,2),(2,3),(3,4),
-    (0,5),(5,6),(6,7),(7,8),
-    (5,9),(9,10),(10,11),(11,12),
-    (9,13),(13,14),(14,15),(15,16),
-    (13,17),(17,18),(18,19),(19,20),
-    (0,17)
-    ]
+    def draw_connections(self, frame):
+        if self.result is None:
+                    return frame
+        height, width, _ = frame.shape
+        
+        for hand in self.result.hand_landmarks:
+            for start, end in self.HAND_CONNECTIONS:
+                x1 = int(hand[start].x * width)
+                y1 = int(hand[start].y * height)
 
-while True:
-    success, frame = cap.read()
+                x2 = int(hand[end].x * width)
+                y2 = int(hand[end].y * height)
 
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
-
-    timestamp_ms = int(time.time() * 1000)
-    result = detector.detect_for_video(mp_image, timestamp_ms)
-    height, width, _ = frame.shape
-
-    for hand in result.hand_landmarks:
-        for id, landmark in enumerate(hand):
-            x = int(landmark.x * width)
-            y = int(landmark.y * height)
-
-            cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
-            cv2.putText(frame, str(id), (x+10, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
 
-    for hand in result.hand_landmarks:
-        for start, end in HAND_CONNECTIONS:
-            x1 = int(hand[start].x * width)
-            y1 = int(hand[start].y * height)
+    def draw_ids(self, frame):
+        if self.result is None:
+                            return frame
+        height, width, _ = frame.shape
+        
+        for hand in self.result.hand_landmarks:
+            for id, landmark in enumerate(hand):
+                x = int(landmark.x * width)
+                y = int(landmark.y * height)
+                cv2.putText(frame, str(id), (x+10, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
-            x2 = int(hand[end].x * width)
-            y2 = int(hand[end].y * height)
 
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-    print(result.hand_landmarks)
+    def draw_frame(self, frame, result):
+        height, width, _ = frame.shape
 
-    cv2.imshow("Webcam", frame)
+        for hand in result.hand_landmarks:
+            for id, landmark in enumerate(hand):
+                x = int(landmark.x * width)
+                y = int(landmark.y * height)
 
-    if cv2.waitKey(1) == 27:
-        break
+                cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+                cv2.putText(frame, str(id), (x+10, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+        for hand in result.hand_landmarks:
+            for start, end in self.HAND_CONNECTIONS:
+                x1 = int(hand[start].x * width)
+                y1 = int(hand[start].y * height)
+
+                x2 = int(hand[end].x * width)
+                y2 = int(hand[end].y * height)
+
+                cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+        return frame
+
+
+    def get_landmarks(self):
+        return self.landmarks
