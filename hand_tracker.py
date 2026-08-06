650ef0f8dcee5e329318f95bd94b303f22f63702
Anastasiia Parfeniuk <anastasiia.parfeniuk@hm.edu>
2026-08-06 13:07:08 +0200

    Move hand_landmarker.task to a separate folder

--- hand_tracker.py
+++ hand_tracker.py
@@ -6,7 +6,7 @@ import time
 
 class HandTracker:
 
-    def __init__(self, model_path='hand_landmarker.task', _num_hands=6):
+    def __init__(self, model_path='models/hand_landmarker.task', _num_hands=6):
         self.base_options = python.BaseOptions(model_asset_path=model_path)
         self.options = vision.HandLandmarkerOptions(base_options=self.base_options, running_mode=vision.RunningMode.VIDEO, num_hands=_num_hands)
         self.detector = vision.HandLandmarker.create_from_options(self.options)
