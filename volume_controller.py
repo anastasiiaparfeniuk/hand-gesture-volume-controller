4bc410f69de6c38091a24944fd1d504ce0dbd349
Anastasiia Parfeniuk <anastasiia.parfeniuk@hm.edu>
2026-08-08 19:39:51 +0200

    Calculate distance between thumb and index fingertips and display it

--- volume_controller.py
+++ volume_controller.py
@@ -1,5 +1,6 @@
 from hand_tracker import HandTracker
 import cv2
+import math
 
 # TODO: Implement the volume control logic using the HandTracker class.
 # 1. get the two fingertips
@@ -11,6 +12,46 @@ import cv2
 # 7. add smoothing to the volume control to avoid sudden jumps in volume level.
 # 8. add a simple safety activation gesture 
 
+def calculate_distance(point1, point2):
+    return math.hypot(point2["x"] - point1["x"], point2["y"] - point1["y"])
+
+
+
 def main():
     hand_tracker = HandTracker()
     cap = cv2.VideoCapture(0)
+
+    while True: 
+        success, frame = cap.read()
+
+        if not success:
+            break
+
+        hand_tracker.detect(frame)
+        landmarks = hand_tracker.landmarks
+
+        if landmarks:
+             hand = landmarks[0]
+             thumb_tip = hand[4]
+             index_tip = hand[8]
+             distance = calculate_distance(thumb_tip, index_tip)
+
+        cv2.putText(frame, f"Distance: {distance:.3f}",(20, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,(255, 255, 255),2
+            )
+
+        cv2.imshow("Webcam", frame)
+        if cv2.waitKey(1) == 27:
+                break
+
+        hand_tracker.draw(frame)
+
+        cv2.imshow("Volume Controller", frame)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
